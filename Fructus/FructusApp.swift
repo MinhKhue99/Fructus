fb506c2fe04489c1a2af223082f19bef0bc74727
KhuePM <minhkhuepham99@gmail.com>
2022-08-27 08:55:12 +0700

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Phạm Minh Khuê on 27/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
